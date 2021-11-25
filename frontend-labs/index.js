d26208781c751e5965553b18eed105cb00749385
Mariana Inoue <marinoue@gmail.com>
2021-11-25 20:36:08 -0300

    front

---
+++ frontend-labs/index.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+  <title>Front - Exemplo</title>
+
+</head>
+
+<body>
+  <header class="header">
+      <h1>Minha loja</h1>
+  </header>
+  <div id="content">
+
+<div class="container">
+    <h1> Delivery</h1>
+     <table id="tabela-lista">
+    </div>
+
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Nome</th>
+        <th>Endereço</th>
+        <th>Telefone</th>
+        <th>Peso</th>
+        <th>Excluir</th>
+      </tr>
+    </thead>
+
+    <tbody id="tabela-lista-corpo"></tbody>
+  </table>
+
+</div>
+
+  <!-- <footer>Feito com ♥</footer> -->
+  <script src="/frontend/js/index.js" type="text/javascript"></script>
+</body>
+
+</html> 
